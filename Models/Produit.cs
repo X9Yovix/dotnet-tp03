ff3df89bcc5297994a2cc2ee9a43a9cab525331e
Karim Ouazzou <karimouazzou@gmail.com>
2024-04-03 19:26:43 +0100

    Création des classes du domaine (Model)

---
+++ Models/Produit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP03.Models
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Prix { get; set; }
+    }
+}
